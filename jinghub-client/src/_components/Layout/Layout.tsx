efbd639076506db7dddb2d540114c41dc62b76af
imnotmoon <imnotmoon@gmail.com>
2022-03-13 19:42:23 +0900

    Change transition effect

--- jinghub-client/src/_components/Layout/Layout.tsx
+++ jinghub-client/src/_components/Layout/Layout.tsx
@@ -10,9 +10,9 @@ type LayoutProps = {
 };
 
 const pageMotion = {
-  initial: {opacity: 0},
-  animate: {opacity: 1, transition: {duration: 2}},
-  exit: {opacity: 0, transition: {duration: 2}}
+  initial: {x: -window.innerWidth, transition: { duration: 1}},
+  animate: {x: 0, transition: {duration: 1}},
+  exit: {x: 200, transition: {duration: 1}}
 };
 
 const Layout: FC<LayoutProps> = ({ children, direction }) => {
